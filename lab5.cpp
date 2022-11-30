64e1e957b6624908f6f2fa42733f3c40338aaf0c
r1zhok top1 <oleksiystrizhuk@gmail.com>
2022-11-30 16:17:42 +0200

    adding my lab

---
+++ lab5.cpp
@@ -0,0 +1,21 @@
+#include "class Clossing.h"
+#include "class Wardrobe.h"
+
+using namespace std;
+
+int main()
+{
+    Clothing A("Gucci", "very nice textile", "Paris", "black", 50, Shirt),
+        B("stani za 40uah", "idk", "China", "white", 70, Jeans),
+        C("Moncler", "Sheeesh", "London", "white", 40, Jacket);
+
+    Wardrobe D;
+    D.Add_Clo(A);
+    D.Add_Clo(B);
+    D.Add_Clo(C);
+    D.goOut();
+    D.Sort();
+
+
+    return 0;
+}
