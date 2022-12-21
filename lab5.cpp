569d33512909e2ba9fefb835a23069f6d5d16fe6
r1zhok top1 <oleksiystrizhuk@gmail.com>
2022-12-21 19:39:57 +0200

    adding remastered cpp file

--- lab5.cpp
+++ lab5.cpp
@@ -5,16 +5,17 @@ using namespace std;
 
 int main()
 {
-    Clothing A("Gucci", "very nice textile", "Paris", "black", 50, Shirt),
-        B("stani za 40uah", "idk", "China", "white", 70, Jeans),
-        C("Moncler", "Sheeesh", "London", "white", 40, Jacket);
+    Clothing Shirt("Gucci", "very nice textile", "Paris", "black", 50, Shirt),
+        Jeans("stani za 40uah", "idk", "China", "white", 70, Jeans),
+        Jacket("Moncler", "Sheeesh", "London", "white", 40, Jacket);
 
-    Wardrobe D;
-    D.Add_Clo(A);
-    D.Add_Clo(B);
-    D.Add_Clo(C);
-    D.goOut();
-    D.Sort();
+    Wardrobe Main;
+
+    Main.add_clo(Shirt);
+    Main.add_clo(Jeans);
+    Main.add_clo(Jacket);
+    Main.go_out();
+    Main.Sort();
 
 
     return 0;
