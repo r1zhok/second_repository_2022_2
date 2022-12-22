8d07a368705ea98b508fe7378e15ebebc1f7cfb3
r1zhok top1 <oleksiystrizhuk@gmail.com>
2022-12-22 10:23:42 +0200

    adding remastered header Clothing.h

--- Clossing.h
+++ Clossing.h
@@ -8,16 +8,14 @@ enum Type { Shirt, Jeans, Jacket };
 string eType[] = { "Shirt", "Jeans", "Jacket" };
 
 class Clothing {
-
-public:
-
+private:
     string name;
     string description;
     string location;
     string color;
     int size;
     Type type;
-
+public:
 
     Clothing(string name, string description, string location, string color, int size, enum Type type) {
         this->name = name;
@@ -26,7 +24,27 @@ public:
         this->color = color;
         this->size = size;
         this->type = type;
+    }
+    string get_name() {
+        return name;
+    }
+    string get_description() {
+        return description;
+    }
+    string get_location() {
+        return location;
+    }
+    string get_color() {
+        return color;
+    }
+    int get_size() {
+        return size;
+    }
+    Type get_type() {
+        return type;
+    }
 
+    void print_clothing() {
         cout << "name = " << this->name << endl;
         cout << "description = " << this->description << endl;
         cout << "location = " << this->location << endl;
